589e1b1263c84abe2c70d7b5128ef9bdeabf440e
bodda <abdelrahman3aysh@hotmail.com>
2021-02-18 03:26:39 +0200

    car

--- js/all.js
+++ js/all.js
@@ -64,6 +64,52 @@ function Tree()
     }
 }
 
+function Car()
+{
+    this.WholeCar = new THREE.Group();
+
+    var CarBody = new THREE.Mesh(
+        new THREE.BoxGeometry(220, 100, 60),
+        new THREE.MeshLambertMaterial({ color: 0xffffff, flatShading: true})
+    );
+    CarBody.translateY(600);
+    CarBody.castShadow = true;
+    CarBody.receiveShadow = true;
+    this.WholeCar.add(CarBody);
+
+    var CarMid = new THREE.Mesh(
+        new THREE.BoxGeometry(130, 100, 50),
+        new THREE.MeshLambertMaterial({ color: 0x4d2fff, flatShading: true})
+    );
+    CarMid.translateY(680);
+    // CarMid.position.z = 50;
+    CarMid.castShadow = true;
+    CarMid.receiveShadow = true;
+    this.WholeCar.add(CarMid);
+
+    var Wheel1 = new THREE.Mesh(
+        new THREE.BoxGeometry(48, 48, 88),
+        new THREE.MeshLambertMaterial({ color: 0x333333, flatShading: true })
+    );
+
+    Wheel1.translateY(550);
+    Wheel1.position.x = -60;
+    Wheel1.position.z = 10;
+
+    var Wheel2 = new THREE.Mesh(
+        new THREE.BoxGeometry(48, 48, 88),
+        new THREE.MeshLambertMaterial({ color: 0x333333, flatShading: true })
+    );
+
+    Wheel2.translateY(550);
+    Wheel2.position.x = 60;
+    Wheel2.position.z = 10;
+
+    this.WholeCar.add(Wheel1);
+    this.WholeCar.add(Wheel2);
+
+}
+
 function CreateSnowman()
 {
     var snowman = new Snowman();
@@ -76,6 +122,13 @@ function CreateTree()
     var tree = new Tree();
     scene.add(tree.WholeTree);
     tree.WholeTree.position.set(0, 200, 0);
+}
+
+function CreateCar()
+{
+    var car = new Car();
+    scene.add(car.WholeCar);
+    car.WholeCar.position.set(0, 200, 0);
 }
 
 function Lights() 
@@ -112,7 +165,8 @@ function init()
     camera.lookAt(new THREE.Vector3(0, 800, 0));
     scene.add(camera);
     // CreateSnowman();
-    CreateTree();
+    // CreateTree();
+    CreateCar();
     Lights();
 
     renderer.render(scene, camera);
