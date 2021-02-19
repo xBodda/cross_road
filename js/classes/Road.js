508509aa984f67c838cc5a540d32201ed47cb9e7
xTrimy <14347568+xTrimy@users.noreply.github.com>
2021-02-19 15:53:21 +0200

    Add car animation and spawning with random times & speeds + Add player controls and animation effects

--- js/classes/Road.js
+++ js/classes/Road.js
@@ -1,7 +1,10 @@
+
+
+
 function Road(LocationZ = 0)
 {
     this.object = new THREE.Group();
-    var sizeX = 10000,
+    var sizeX = 20000,
         sizeZ = 600;
     var Plane = new THREE.Mesh(
         new THREE.PlaneGeometry(sizeX, sizeZ),
@@ -13,4 +16,29 @@ function Road(LocationZ = 0)
     this.object.add(Plane);
     this.object.position.set(0,0,LocationZ);
     return this.object;
+}
+/**  @params roadMap: 1 = road, 0 = Resting Ground */
+var roadsMap = [1,0,0,1,1,0,1,0,1];
+var roads = [];
+function createRoads(startingPositionZ = 0){
+/**
+ * @Params startingPositionZ: the position of the starting platform
+ * We subtract 600 as it's the size of the starting platform
+ */
+    for(let i = 0; i<roadsMap.length; i++){
+        if(roadsMap[i]){
+            let road = new Road(startingPositionZ - 600);
+            scene.add(road);
+            var roadObj = 
+            {
+                road:road,
+                direction:Math.floor(Math.random()*1+0.5),
+                difficulty:Math.ceil(Math.random()*10)*100
+            };
+            roads.push(roadObj)
+        }
+        startingPositionZ-=600;
+    }
+    SpawnCars()
+    
 }
